20d19c664033e0b7f9ec2e7627cc43be2d37bde4
Faisal Althqfi <faisalalthqfi@Faisals-MacBook-Pro.local>
2024-02-24 03:42:39 +0300

    v1

---
+++ src/main/java/com/example/hogwartsartifacts/HogwartsArtifactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hogwartsartifacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtifactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hogwartsartifacts/HogwartsArtifactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwartsartifacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
